8113dc7fd11bb2e906f2aeaccc6f37fd0d210145
Andressa <andressaszx@gmail.com>
2020-04-02 13:17:49 -0300

    Adição do método calcular area na classe Triangulo

--- src/main/java/com/mycompany/projeto2_aula62/Program.java
+++ src/main/java/com/mycompany/projeto2_aula62/Program.java
@@ -1,25 +1,8 @@
 
 package com.mycompany.projeto2_aula62;
-import com.mycompany.projeto2_aula62.Triangulo;
-import java.util.Locale;
-
-/* 
- * Fazer um programa para ler as medidas dos lados de
- * dois triangulos X e Y (suponha medidas válidas).
- * Em seguida, mostrar o valor das áreas dos dois triângulos
- * e dizer qual dos dois triângulos possui a maior área.
- * area = raiz p * (p-a) * (p-b) * (p-c)
- * p = (a+b+c)/2
- */
 
 import java.util.Locale;
 import java.util.Scanner;
-
-/**
- *
- * @author Andressa Silva
- * Solucao 1, paradigma estruturado
- */
 
 public class Program {
     
@@ -27,9 +10,6 @@ public class Program {
         
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
-        
-        double xA, xB, xC, yA, yB, yC;
-        double areaX, areaY;
         
         Triangulo x, y;
         x = new Triangulo();
@@ -51,11 +31,8 @@ public class Program {
         System.out.print("Informe o lado 3: ");
         y.c = input.nextDouble();
         
-        double pX = (x.a + x.b + x.c)/2.0;
-        areaX = Math.sqrt(pX * (pX-x.a) * (pX-x.b) * (pX-x.c));
-        
-        double pY = (y.a + y.b + y.c)/2.0;
-        areaY = Math.sqrt(pY * (pY - y.a) * (pY - y.b) * (pY - y.c));
+        double areaX = x.area();
+        double areaY = y.area();
         
         if(areaX > areaY){
             System.out.printf("Area do triangulo X = %.2f maior que area de Y %.2f ", areaX, areaY);
--- src/main/java/com/mycompany/projeto2_aula62/Triangulo.java
+++ src/main/java/com/mycompany/projeto2_aula62/Triangulo.java
@@ -9,10 +9,16 @@ package com.mycompany.projeto2_aula62;
  *
  * @author Andressa Silva
  */
+
 public class Triangulo {
     
     public double a;
     public double b;
     public double c;
     
+    public double area(){
+        double p = (a + b + c)/2.0;
+        double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return result;
+    }
 }
