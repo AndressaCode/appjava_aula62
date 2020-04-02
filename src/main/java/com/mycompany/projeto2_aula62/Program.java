404826a0240fc6c92c1dfdf7c7e793889d19a4e4
Andressa <andressaszx@gmail.com>
2020-04-02 12:58:22 -0300

    Adicao da classe Triangulo

--- src/main/java/com/mycompany/projeto2_aula62/Program.java
+++ src/main/java/com/mycompany/projeto2_aula62/Program.java
@@ -1,4 +1,9 @@
-/* UTILIZANDO PARADIGMA ESTRUTURADO
+
+package com.mycompany.projeto2_aula62;
+import com.mycompany.projeto2_aula62.Triangulo;
+import java.util.Locale;
+
+/* 
  * Fazer um programa para ler as medidas dos lados de
  * dois triangulos X e Y (suponha medidas válidas).
  * Em seguida, mostrar o valor das áreas dos dois triângulos
@@ -7,16 +12,17 @@
  * p = (a+b+c)/2
  */
 
-package com.mycompany.projeto2_aula62;
-
 import java.util.Locale;
 import java.util.Scanner;
 
 /**
  *
  * @author Andressa Silva
+ * Solucao 1, paradigma estruturado
  */
+
 public class Program {
+    
     public static void main(String[] args){
         
         Locale.setDefault(Locale.US);
@@ -25,27 +31,31 @@ public class Program {
         double xA, xB, xC, yA, yB, yC;
         double areaX, areaY;
         
+        Triangulo x, y;
+        x = new Triangulo();
+        y = new Triangulo();
+        
         System.out.println("LADOS DO TRIANGULO X");
         System.out.print("Informe o lado 1: ");
-        xA = input.nextDouble();
+        x.a = input.nextDouble();
         System.out.print("Informe o lado 2: ");
-        xB = input.nextDouble();
+        x.b = input.nextDouble();
         System.out.print("Informe o lado 3: ");
-        xC = input.nextDouble();
+        x.c = input.nextDouble();
         
         System.out.println("LADOS DO TRIANGULO Y");
         System.out.print("Informe o lado 1: ");
-        yA = input.nextDouble();
+        y.a = input.nextDouble();
         System.out.print("Informe o lado 2: ");
-        yB = input.nextDouble();
+        y.b = input.nextDouble();
         System.out.print("Informe o lado 3: ");
-        yC = input.nextDouble();
+        y.c = input.nextDouble();
         
-        double pX = (xA + xB + xC)/2.0;
-        areaX = Math.sqrt(pX * (pX-xA) * (pX-xB) * (pX-xC));
+        double pX = (x.a + x.b + x.c)/2.0;
+        areaX = Math.sqrt(pX * (pX-x.a) * (pX-x.b) * (pX-x.c));
         
-        double pY = (yA + yB + yC)/2.0;
-        areaY = Math.sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+        double pY = (y.a + y.b + y.c)/2.0;
+        areaY = Math.sqrt(pY * (pY - y.a) * (pY - y.b) * (pY - y.c));
         
         if(areaX > areaY){
             System.out.printf("Area do triangulo X = %.2f maior que area de Y %.2f ", areaX, areaY);
---
+++ src/main/java/com/mycompany/projeto2_aula62/Triangulo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projeto2_aula62;
+
+/**
+ *
+ * @author Andressa Silva
+ */
+public class Triangulo {
+    
+    public double a;
+    public double b;
+    public double c;
+    
+}
